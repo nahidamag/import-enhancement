d78130ff3b19aeaa3e6874f71fd0401b9530f8ac
nahidamag <nahid@amagroup.io>
2022-02-01 22:33:12 +0600

    bug-fixed

--- src/components/Layout/ImportConfigLayout.tsx
+++ src/components/Layout/ImportConfigLayout.tsx
@@ -2,6 +2,7 @@ import { Box } from '@mui/material';
 
 interface IProps {}
 
+
 const ImportConfigLayout: React.FC<IProps> = ({ children }) => {
   return (
     <Box sx={{ display: 'flex', width: '100vw', backgroundColor: '#fff' }}>
